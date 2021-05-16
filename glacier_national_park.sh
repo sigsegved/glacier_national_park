e22e6a5b906964f208cd0c5bc80cf1adb2f7d65b
Karthik <1487102+sigsegved@users.noreply.github.com>
2021-05-15 17:29:21 -0700

    Add files via upload

--- glacier_national_park.sh
+++ glacier_national_park.sh
@@ -14,7 +14,12 @@ do
   end_date="$month/$next/2021"
   for hotel_code in GLLM GLMG GLRS GLSC GLVI GLCC 
   do
-      units_available=`curl -s -G --data-urlencode "hotel_code=$hotel_code" --data-urlencode "dateFrom=$start_date" --data-urlencode "dateTo=$end_date" --data-urlencode "adults=2" --data-urlencode "children=0" $base_url | jq -r '.rateplans.rateplans' | jq length`
+      units_available=`curl -s -G --data-urlencode "hotel_code=$hotel_code"\
+	               	--data-urlencode "dateFrom=$start_date"\
+			--data-urlencode "dateTo=$end_date"\
+			--data-urlencode "adults=2"\
+			--data-urlencode "children=0"\
+			$base_url | jq -r '.rateplans.rateplans' | jq length`
       if [ $units_available -gt 0 ];
       then
       	echo "$hotel_code\t$start_date\t$units_available";
