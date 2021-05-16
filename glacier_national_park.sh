78054aa7f3c3ce240aa1dba6a94e4ad8352e8c14
Karthik <1487102+sigsegved@users.noreply.github.com>
2021-05-15 17:08:01 -0700

    Get all national park lodge availability in GNP
    Outputs the availability in tab separated format. 
    `HOTEL_CODE | DATE | UNITS AVAILABLE`
    
    Example output..
    ```
    GLVI	8/27/2021	0
    GLCC	8/27/2021	0
    GLLM	8/28/2021	0
    GLMG	8/28/2021	0
    GLRS	8/28/2021	0
    GLSC	8/28/2021	0
    ```

---
+++ glacier_national_park.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+start=$1
+month=$2
+end=$3
+
+base_url="https://secure.glaciernationalparklodges.com/lodging/getRooms";
+
+cur=$start
+while [ $cur -lt $end ]
+do
+  start_date="$month/$cur/2021"
+  next=$(($cur+1))
+  end_date="$month/$next/2021"
+  for hotel_code in GLLM GLMG GLRS GLSC GLVI GLCC 
+  do
+      units_available=`curl -s -G --data-urlencode "hotel_code=$hotel_code" --data-urlencode "dateFrom=$start_date" --data-urlencode "dateTo=$end_date" --data-urlencode "adults=2" --data-urlencode "children=0" $base_url | jq -r '.rateplans.rateplans' | jq length`
+      echo "$hotel_code\t$start_date\t$units_available";
+  done
+  cur=$next
+done
+
