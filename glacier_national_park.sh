bcd7a68bf9abd5b7bd2a1767ca216d7ace0853cc
Karthik <1487102+sigsegved@users.noreply.github.com>
2021-05-15 17:26:43 -0700

    Print only available dates

--- glacier_national_park.sh
+++ glacier_national_park.sh
@@ -15,7 +15,10 @@ do
   for hotel_code in GLLM GLMG GLRS GLSC GLVI GLCC 
   do
       units_available=`curl -s -G --data-urlencode "hotel_code=$hotel_code" --data-urlencode "dateFrom=$start_date" --data-urlencode "dateTo=$end_date" --data-urlencode "adults=2" --data-urlencode "children=0" $base_url | jq -r '.rateplans.rateplans' | jq length`
-      echo "$hotel_code\t$start_date\t$units_available";
+      if [ $units_available -gt 0 ];
+      then
+      	echo "$hotel_code\t$start_date\t$units_available";
+      fi
   done
   cur=$next
 done
